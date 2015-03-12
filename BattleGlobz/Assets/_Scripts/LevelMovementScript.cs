3d92ea833f9459982c1ebf7aae615de890c6bae2
Alex <alancast@umich.edu>
2015-03-12 13:01:03 -0400

    got the level moving and camera static

---
+++ BattleGlobz/Assets/_Scripts/LevelMovementScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMovementScript : MonoBehaviour {
+	public static bool	stopMoving = false;
+	float 				moveAmount = .01f;
+
+	void FixedUpdate(){
+		if (stopMoving){
+			return;
+		}
+		Vector3 nextPosition = transform.position;
+		nextPosition.x -= moveAmount;			
+		transform.position = nextPosition;
+	}
+}
