8bbf7b9d57ad602edd69a1a80c91f74db1ff580a
Alex <alancast@umich.edu>
2015-04-01 00:57:21 -0400

    set level time back to 75

--- BattleGlobz/Assets/_Scripts/CameraScript.cs
+++ BattleGlobz/Assets/_Scripts/CameraScript.cs
@@ -8,7 +8,7 @@ public class CameraScript : MonoBehaviour {
 	//true if there is a boss false if none
 	static public bool			isBoss = false;
 	//time of level until it ends
-	float 			levelTime = 5;
+	float 			levelTime = 75;
 	//text for number of kills and time
 	//public so that it can be set from BossScript when game ends
 	public Text		timeText;
