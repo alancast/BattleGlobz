af841943434bca4a7c0638dbbedf5e5d7e424ac1
Jake Glass <snakehockey2@gmail.com>
2015-04-24 16:25:12 -0400

    created mm2

---
+++ BattleGlobz/Assets/_Scripts/MenuScript1.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class MenuScript1 : MonoBehaviour {
+	public SpriteRenderer controlsSprite;
+	public SpriteRenderer dpadSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var gameController = InputManager.ActiveDevice;
+		
+
+		if (gameController.DPadUp.WasPressed){
+			controlsSprite.enabled = true;
+			Color newC = dpadSprite.color;
+			newC.a = 0f;
+			dpadSprite.color = newC;
+
+		} else if (gameController.DPadUp.WasReleased){
+			controlsSprite.enabled = false;
+			Color newC = dpadSprite.color;
+			newC.a = 1f;
+			dpadSprite.color = newC;
+
+		}
+
+		if (gameController.DPadLeft.WasPressed){
+			Application.LoadLevel("_Scene_2PlayerB");
+
+		}
+		if (gameController.DPadDown.WasPressed){
+			Application.LoadLevel("_Scene_3Player");
+
+		}
+		if (gameController.DPadRight.WasPressed){
+			Application.LoadLevel("_Scene_4Player");
+
+		}
+
+
+	}
+}
